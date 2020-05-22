a8ad7c16cd646c4fb4b8d5c389c3427e0199c98a
josealfarow <josealberto.alfaro@ucr.ac.cr>
2020-05-21 23:22:59 -0600

    executable files

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys # pip install cx_Freeze
+from cx_Freeze import setup, Executable
+
+include_files = ['autorun.inf']
+nase = None
+
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="Proyecto_ParteII",
+      version="1.0",
+      description="Segunda parte del proyecto de RI",
+      options={'build_exe': {'include_files': include_files}},
+      executables={Executable("main.py", base=base)})
